a01bb2abb9bd9b3d93bdb3928530e55bcc2bc776
Gabriel <gabrielborc@gmail.com>
2022-02-18 00:23:15 -0300

    initial commit

---
+++ app/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+}
